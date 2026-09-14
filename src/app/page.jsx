e146c694c2f40185c27850bdd69c1bd517ea483c
Beatriz Miotto <miotto.beatriz2008@gmail.com>
2026-09-14 11:12:27 -0300

    inicia template

---
+++ src/app/page.jsx
@@ -0,0 +1,28 @@
+import { examples } from "@/data/crud";
+import styles from './page.module.css';
+import Card from '@components/Card';
+
+export default async function Page() {
+    
+    return (
+        <>
+        <main className={styles.main}>
+            {examples.map(({ id, method, verb, description, color, Icon }) => (
+                <Card
+                key={id}
+                id={id}
+                verb={verb}
+                method={method}
+                description={description}
+                color={color}
+                Icon={Icon}
+                />
+            ))}
+        </main>
+        <footer className={styles.footer}>
+            <p>Codeverse &copy; {new Date().getFullYear()}</p>
+            <p>Next.js - Axios - Ant Design - Lucide React</p>
+        </footer>
+        </>
+    )
+}
